7ca3159e78b44c6dbe13f1d545529d70cebfc8b9
LainePittman <142843923+LainePittman@users.noreply.github.com>
2024-03-07 20:12:45 -0600

    Update person.py

--- person.py
+++ person.py
@@ -59,7 +59,7 @@ def BMIcat (BMI):
     if BMI < 18.5:
         Person.category = "Underweight"
     
-    elif 18.6 <= BMI <= 24.9:
+    elif 18.5 <= BMI <= 24.9:
         Person.category = "Normal Weight"
 
     elif 25 <= BMI <= 29.9:
